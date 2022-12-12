9279854db62883036c55deec7c3ce00e4ed8aeaf
CoolPotatoStas <gbrpotato@gmail.com>
2022-12-12 17:12:01 +0300

    beta

--- index.php
+++ index.php
@@ -4,13 +4,11 @@ require_once 'scripts/template.php';
 if (isset($_GET['type'])) {
 	require_once 'scripts/filters.php';
 	$products = get_products_by_type($_GET['type']);
-//	$productList = template('views/products.php', ['products' => $products]);
     $productList = template('views/table.php', ['products' => $products]);
 
 } else {
 	require_once 'scripts/filters.php';
     $products = get_products_by_type(1);
-//	$productList = template('views/products.php', ['products' => $products]);
     $productList = template('views/table.php', ['products' => $products]);
 }
 
